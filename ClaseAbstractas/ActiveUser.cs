c895c2f2d790ed2197d840ee3ba0da8840bbdd48
Abel <Abel@ABELREYNALTE>
2018-06-16 10:11:31 -0500

    Agregar archivos de proyecto.

---
+++ ClaseAbstractas/ActiveUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstractas
+{
+    public class ActiveUser: User
+    {
+        public override void Login()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ ClaseAbstractas/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAbstractas
+{
+    public abstract class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int AccountBalance { get; set; }
+
+        public abstract void Login();
+    }
+}
